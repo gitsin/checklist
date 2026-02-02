5c1d2c2aedf91876dc9a3b39eeb78d2a34461798
gitsin <flavio.gitsin@gmail.com>
2026-02-02 18:30:40 -0300

    teste temporio supal

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 // TESTE TEMPORÁRIO
 const supabaseUrl = 'https://jeuxaiovxfxfbbrnydef.supabase.co'
-const supabaseAnonKey = 'sua-sb_secret_u2mPSVjZhIa_10tIzxnkSg_czgXiuFk-real-aqui'
+const supabaseAnonKey = 'sb_secret_Mzz3l0gkwEvyvKI9c4QZQw_fGK9OMVn'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
